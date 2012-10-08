c28a223bd2bba22327491fc965774482d3ce6a42
jacobsin <jacob.sin@gmail.com>
2012-10-08 08:25:24 +0800

    some googling phantomjs

--- app/google.js
+++ app/google.js
@@ -1,25 +1,23 @@
 // Google for hong kong weather
 
 var page = require('webpage').create(),
-    url = 'http://www.google.com.hk';
+  url = 'http://www.google.com.hk';
 
-page.onConsoleMessage = function(msg) { console.log(msg); }
+page.onConsoleMessage = function (msg) {
+  console.log(msg);
+};
 
 page.open(url, function (status) {
-    if (status !== 'success') {
-        console.log('Unable to access network');
-    } else {
-            // jQuery is loaded, now manipulate the DOM
-            page.evaluate(function () {
-//                var q = $('input[name=q]');
-//                q.val('hong kong weather');
-
-                var q = document.querySelector('input[name=q]');
-                q.focus();
-                q.value = 'hong kong weather';
-            });
-//        });
-        page.render("images/step.png");
-    }
-    phantom.exit();
+  if (status !== 'success') {
+    console.log('Unable to access network');
+  } else {
+    // jQuery is loaded, now manipulate the DOM
+    page.evaluate(function () {
+      var q = document.querySelector('input[name=q]');
+      q.focus();
+      q.value = 'hong kong weather';
+    });
+    page.render("images/step.png");
+  }
+  phantom.exit();
 });
